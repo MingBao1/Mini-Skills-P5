195a9ccbb780efd66f03106f217bc7756f84eab7
Ming Bao <18124mb@hvhs.school.nz>
2021-02-15 07:15:55 +0000

    Put the speedX and speedY variables into the ball objects

--- t07_objects.js
+++ t07_objects.js
@@ -3,17 +3,18 @@
 /********************************************************/
 var ball1 = {
   x : 0,
-  y : 0
+  y : 0,
+  speedX : 3,
+  speedY : 4
 }
-var ball1SpeedX = 3;
-var ball1SpeedY = 4;
+
 
 var ball2 = {
   x : 0,
-  y : 0 
+  y : 0, 
+  speedX : 3,
+  speedY : 4
 }
-var ball2SpeedX = 3;
-var ball2SpeedY = 4;
 
 function setup() {
   createCanvas(400, 400);
@@ -25,30 +26,30 @@ function draw() {
 /******************First ball**********************************/
   ellipse (ball1.x, ball1.y, 30, 30);
 
-  ball1.x = ball1.x + ball1SpeedX;
-  ball1.y = ball1.y + ball1SpeedY;
+  ball1.x = ball1.x + ball1.speedX;
+  ball1.y = ball1.y + ball1.speedY;
 
   if (ball1.x >= width || ball1.y >= height) {
-    ball1SpeedX = -random (6);
-    ball1SpeedY = -random (6); 
+    ball1.speedX = -random (6);
+    ball1.speedY = -random (6); 
   }
   else if (ball1.x <= 0 || ball1.y <= 0) {
-    ball1SpeedX = random (6); 
-    ball1SpeedY = random (6);
+    ball1.speedX = random (6); 
+    ball1.speedY = random (6);
   } 
 /************************Second Ball************************************/
   ellipse (ball2.x, ball2.y, 30, 30);
 
-  ball2.x = ball2.x + ball2SpeedX;
-  ball2.y = ball2.y + ball2SpeedY;
+  ball2.x = ball2.x + ball2.speedX;
+  ball2.y = ball2.y + ball2.speedY;
 
   if (ball2.x >= width || ball2.y >= height) {
-    ball2SpeedX = -random (6);
-    ball2SpeedY = -random (6); 
+    ball2.speedX = -random (6);
+    ball2.speedY = -random (6); 
   }
   else if (ball2.x <= 0 || ball2.y <= 0) {
-    ball2SpeedX = random (6); 
-    ball2SpeedY = random (6);
+    ball2.speedX = random (6); 
+    ball2.speedY = random (6);
   } 
 
 }
