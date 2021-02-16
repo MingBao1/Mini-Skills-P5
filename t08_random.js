d74141986a1491a1779a22e9cae0ab1707dcb825
Ming Bao <18124mb@hvhs.school.nz>
2021-02-16 01:18:33 +0000

    Fixed bouncing problem where the ball can't bounce above 90 degrees.

--- t08_random.js
+++ t08_random.js
@@ -31,53 +31,84 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  //background(220);
 
 /******************First ball**********************************/
   fill (ball1.colR, ball1.colG, ball1.colB);
+  noStroke();
   ellipse (ball1.x, ball1.y, 30, 30);
 
   ball1.x = ball1.x + ball1.speedX;
   ball1.y = ball1.y + ball1.speedY;
 
-  if (ball1.x >= width || ball1.y >= height) {
-    ball1.speedX = -random (6);
-    ball1.speedY = -random (6); 
+  if (ball1.x >= width) {
+    ball1.speedX = -random (6); 
 
     ball1.colR = random (0, 255);
     ball1.colG = random (0, 255);
     ball1.colB = random (0, 255);
   }
-  else if (ball1.x <= 0 || ball1.y <= 0) {
+  else if (ball1.x <= 0) {
     ball1.speedX = random (6); 
-    ball1.speedY = random (6);
 
     ball1.colR = random (0, 255);
     ball1.colG = random (0, 255);
     ball1.colB = random (0, 255);
   } 
+    else if (ball1.y >= height) {
+    ball1.speedY = -random (6); 
+    ball1.speedX = random (-6, 6);
+
+    ball1.colR = random (0, 255);
+    ball1.colG = random (0, 255);
+    ball1.colB = random (0, 255);
+  }
+  else if(ball1.y <= 0) {
+    ball1.speedY = random (6);
+    ball1.speedX = random (-6, 6);
+
+    ball1.colR = random (0, 255);
+    ball1.colG = random (0, 255);
+    ball1.colB = random (0, 255);
+  }
+
 /************************Second Ball************************************/
   fill (ball2.colR, ball2.colG, ball2.colB);
+  noStroke();
   ellipse (ball2.x, ball2.y, 30, 30);
 
   ball2.x = ball2.x + ball2.speedX;
   ball2.y = ball2.y + ball2.speedY;
 
-  if (ball2.x >= width || ball2.y >= height) {
+  if (ball2.x >= width) {
     ball2.speedX = -random (6);
-    ball2.speedY = -random (6); 
 
     ball2.colR = random (0, 255);
     ball2.colG = random (0, 255);
     ball2.colB = random (0, 255);
   }
-  else if (ball2.x <= 0 || ball2.y <= 0) {
+  else if (ball2.x <= 0) {
     ball2.speedX = random (6); 
-    ball2.speedY = random (6);
 
     ball2.colR = random (0, 255);
     ball2.colG = random (0, 255);
     ball2.colB = random (0, 255);
   } 
+  else if (ball2.y >= height) {
+    ball2.speedY = -random (6); 
+    ball2.speedX = random (-6, 6);
+
+    ball2.colR = random (0, 255);
+    ball2.colG = random (0, 255);
+    ball2.colB = random (0, 255);
+  }
+  else if(ball2.y <= 0) {
+    ball2.speedY = random (6);
+    ball2.speedX = random (-6, 6);
+
+    ball2.colR = random (0, 255);
+    ball2.colG = random (0, 255);
+    ball2.colB = random (0, 255);
+  }
 
 }
