b563e562242b0847eac698af0d8461ad47dbb1e0
Ming Bao <18124mb@hvhs.school.nz>
2021-02-22 00:55:34 +0000

    Made a failed verson of the slider thing

--- t09_functions.js
+++ t09_functions.js
@@ -1,158 +1,56 @@
 /********************************************************/
 //
 /********************************************************/
-/*var ball1 = {
-  dia : 30,
-  rad : 0,
-  x : false,
-  y : false,
-  speedX : 3,
-  speedY : 4,
-  colR : 0,
-  colG : 0,
-  colB : 0
-};
-
-
-var ball2 = {
-  dia : 30,
-  rad : 0,
-  x : false,
-  y : false, 
-  speedX : 3,
-  speedY : 4,
-  colR : 0,
-  colG : 0,
-  colB : 0
-}; */
-
-var slider;
+var slider
+var ballXY = {
+  X:0,
+  Y:0,
+}
 
 function setup() {
   createCanvas(400, 400);
-  //frameRate(1);
-
-  ball1.rad = ball1.dia/2;
-  ball2.rad = ball2.dia/2;
-
-  ball1.x = random (ball1.rad, 400);
-  ball1.y = random (ball1.rad, 400);
-  ball2.x = random (ball2.rad, 400);
-  ball2.y = random (ball2.rad, 400);
-
-  slider = sliderFunc();
+  //frameRate(100);
+  createSliderFunc();
 }
 
 function draw() {
-  background(220, 200, 200);
-
-  ball1Func();
-  ball2Func();
-
+  background(200, 100, 20);
+  ballDia = slider.value();
+  ballMove(ballXY.X, ballXY.Y, 3, 4);
+  ballCreate(ballXY.X, ballXY.Y, ballDia);
 }
 
-function sliderFunc() {
-  slider = createSlider (50, 200);
-  slider.position(0, 0);
-  return(slider);
+function createSliderFunc() {
+  slider = createSlider(30, 200, 50);
+  slider.position (0, 200);
 }
 
-function ball1Func(x, y, speedX, speedY, dia, r, g, b){
-  fill (ball1.colR, ball1.colG, ball1.colB);
-  noStroke();
-  ellipse (ball1.x, ball1.y, ball1.dia, ball1.dia);
-
-  ball1.x = ball1.x + ball1.speedX;
-  ball1.y = ball1.y + ball1.speedY;
-
-  if (ball1.x >= width - ball1.rad) {
-    ball1.speedX = -random (6); 
-    ball1.speedY = random (-6, 6);
-
-    ball1.x = width - ball1.rad;
-
-    ball1.colR = random (0, 255);
-    ball1.colG = random (0, 255);
-    ball1.colB = random (0, 255);
-  }
-  else if (ball1.x <= 0 + ball1.rad) {
-    ball1.speedX = random (6); 
-    ball1.speedY = random (-6, 6);
-
-    ball1.x = ball1.rad;
-
-    ball1.colR = random (0, 255);
-    ball1.colG = random (0, 255);
-    ball1.colB = random (0, 255);
-  } 
-    else if (ball1.y >= height - ball1.rad) {
-    ball1.speedY = -random (6); 
-    ball1.speedX = random (-6, 6);
-
-    ball1.y = height - ball1.rad;
-
-    ball1.colR = random (0, 255);
-    ball1.colG = random (0, 255);
-    ball1.colB = random (0, 255);
-  }
-  else if(ball1.y <= 0 + ball1.rad) {
-    ball1.speedY = random (6);
-    ball1.speedX = random (-6, 6);
-
-    ball1.y = ball1.rad;
-
-    ball1.colR = random (0, 255);
-    ball1.colG = random (0, 255);
-    ball1.colB = random (0, 255);
-  }
+function ballCreate(x, y, ballDia) {
+  ellipse(x, y, ballDia);
 }
 
-function ball2Func() {
-  fill (ball2.colR, ball2.colG, ball2.colB);
-  noStroke();
-  ellipse (ball2.x, ball2.y, ball2.dia, ball2.dia);
-
-  ball2.x = ball2.x + ball2.speedX;
-  ball2.y = ball2.y + ball2.speedY;
-
-  if (ball2.x >= width - ball2.rad) {
-    ball2.speedX = -random (6);
-    ball2.speedY = random (-6, 6);
-
-    ball2.x = width - ball2.rad;
-
-    ball2.colR = random (0, 255);
-    ball2.colG = random (0, 255);
-    ball2.colB = random (0, 255);
+function ballMove(x, y, speedX, speedY, dia) {
+  if (x >= width - dia/2) {
+    speedX = speedX * -1;
+    x = width - dia/2;
   }
-  else if (ball2.x <= 0 + ball2.rad) {
-    ball2.speedX = random (6); 
-    ball2.speedY = random (-6, 6);
 
-    ball2.x = ball2.rad;
-
-    ball2.colR = random (0, 255);
-    ball2.colG = random (0, 255);
-    ball2.colB = random (0, 255);
-  } 
-  else if (ball2.y >= height - ball2.rad) {
-    ball2.speedY = -random (6); 
-    ball2.speedX = random (-6, 6);
-
-    ball2.y = height - ball2.rad;
-
-    ball2.colR = random (0, 255);
-    ball2.colG = random (0, 255);
-    ball2.colB = random (0, 255);
+  else if (x <= 0 + dia/2) {
+    speedX = speedX * -1;
+    x = dia/2;
   }
-  else if(ball2.y <= 0 + ball2.rad) {
-    ball2.speedY = random (6);
-    ball2.speedX = random (-6, 6);
 
-    ball2.y = ball2.rad;
-
-    ball2.colR = random (0, 255);
-    ball2.colG = random (0, 255);
-    ball2.colB = random (0, 255);
+  else if (y  >= width - dia/2) {
+    speedY = speedY * -1;
+    y = width - dia/2;
   }
+
+  else if (y <= 0 + dia/2) {
+    speedY = speedY * -1;
+    y = dia/2
+  }
+
+  ballXY.X = x + speedX;
+  ballXY.Y = y + speedY;
+
 }
