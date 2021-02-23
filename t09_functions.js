9153c4874d39c384a6c96e0f4868ddf5f1dc669d
Ming Bao <18124mb@hvhs.school.nz>
2021-02-23 01:42:16 +0000

    Fixed task 9 and made it work

--- t09_functions.js
+++ t09_functions.js
@@ -2,55 +2,83 @@
 //
 /********************************************************/
 var slider
-var ballXY = {
-  X:0,
-  Y:0,
+var ball = {
+  dia: 0,
+  rad:0,
+  x:0,
+  y:0,
+  velX:0,
+  velY:0,
+  r:0,
+  g:0,
+  b:0,
+
+  init:function() {
+    this.x = random(this.rad, width - this.rad);
+    this.y = random(this.rad, width - this.rad);
+
+    this.velX = random(-10, 10);
+    this.velY = random(-10, 10);
+
+  },
+
+  sliderUpdate:function(){
+    this.dia = slider.value();
+    this.rad = this.dia/2;
+  },
+
+  move:function() {
+    this.x = this.x + this.velX;
+    this.y = this.y + this.velY;
+  },
+
+  bounce:function() {
+
+  if (this.x >= width - this.rad) {
+    this.velX = this.velX * -1;
+    this.x = width - this.rad;
+  }
+
+  else if (this.x <= 0 + this.rad) {
+    this.velX = this.velX * -1;
+    this.x = this.rad;
+  }
+
+  else if (this.y  >= width - this.rad) {
+    this.velY = this.velY * -1;
+    this.y = width - this.rad;
+  }
+
+  else if (this.y <= 0 + this.rad) {
+    this.velY = this.velY * -1;
+    this.y = this.rad;
+  }
+  },
+
+  disp:function(){
+    fill (this.r, this.g, this.b);
+    ellipse(this.x, this.y, this.dia);
+  },
 }
 
 function setup() {
   createCanvas(400, 400);
-  //frameRate(100);
+  //frameRate(1);
   createSliderFunc();
+  ball.init();
 }
 
 function draw() {
   background(200, 100, 20);
   ballDia = slider.value();
-  ballMove(ballXY.X, ballXY.Y, 3, 4);
-  ballCreate(ballXY.X, ballXY.Y, ballDia);
+
+  ball.sliderUpdate();
+  ball.move();
+  ball.bounce();
+  ball.disp();
 }
 
 function createSliderFunc() {
   slider = createSlider(30, 200, 50);
   slider.position (0, 200);
-}
-
-function ballCreate(x, y, ballDia) {
-  ellipse(x, y, ballDia);
-}
-
-function ballMove(x, y, speedX, speedY, dia) {
-  if (x >= width - dia/2) {
-    speedX = speedX * -1;
-    x = width - dia/2;
-  }
-
-  else if (x <= 0 + dia/2) {
-    speedX = speedX * -1;
-    x = dia/2;
-  }
-
-  else if (y  >= width - dia/2) {
-    speedY = speedY * -1;
-    y = width - dia/2;
-  }
-
-  else if (y <= 0 + dia/2) {
-    speedY = speedY * -1;
-    y = dia/2
-  }
-
-  ballXY.X = x + speedX;
-  ballXY.Y = y + speedY;
-
 }
